81b04616871069f0b2cb9fca16409a0e2a4c9dae
Robert <robertkng104@gmail.com>
2019-01-26 23:27:10 +0000

    Added co2 information

--- backend/models.py
+++ backend/models.py
@@ -1,11 +1,61 @@
-from . import mongo_db, app
+import requests
+
+from . import mongo_db, app, json
 from datetime import datetime
 from .edmam_wrapper import receipe_lookup
+from werkzeug.security import generate_password_hash, check_password_hash
+import math
+
+
+class User(mongo_db.Document):
+    username = mongo_db.StringField(required=True, unique=True)
+    first_name = mongo_db.StringField(required=True)
+    last_name = mongo_db.StringField(required=True)
+    password = mongo_db.StringField()
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+
+def get_distance(a, b):
+    R = 6371
+    delta_lat = (a[0] - b[0]) * (math.pi / 180)
+    delta_long = (a[1] - b[1]) * (math.pi / 180)
+    a = math.sin(delta_lat / 2) ** 2 + math.cos(a[0]) * math.cos(b[0]) * (math.sin(delta_long / 2) ** 2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    return R * c
 
 
 class Food(mongo_db.Document):
     name = mongo_db.StringField(required=True)
-    expiry = mongo_db.DateTimeField()
+    barcode = mongo_db.IntegerField()
+    country_of_origin = mongo_db.PointField()
+    expiry_advice = mongo_db.StringField()
+    packaging = mongo_db.FloatField()
+    co2_emission_base = mongo_db.FloatField()
+    FOOD_EMISSION_SITE = "https://opendata.socrata.com/resource/8nz9-yn2p.json"
+
+    def save(self, *args, **kwargs):
+        # Trawl site for info if it is available
+        header = {'X-App-Token': app.config['SOCRATA_APP_TOKEN']}
+        params = {'food': self.name.capitalise()}
+        resp = requests.get(self.FOOD_EMISSION_SITE, params=params, header=header)
+        if resp.status_code == 200:
+            food_data = json.loads(resp.text)[0]
+            self.co2_emission_base = float(food_data["grams_co2e_per_serving"])
+        super(Food, self).save(*args, **kwargs)
+
+    def get_recipe(self):
+        return receipe_lookup(self.name)
+
+
+class UserFood(mongo_db.Document):
+    food = mongo_db.ReferenceField(Food, required=True, reverse_delete_rule=mongo_db.DENY)
+    expiry = mongo_db.DateTimeField(required=True)
+    owner = mongo_db.ReferenceField(User, required=True, reverse_delete_rule=mongo_db.CASCADE)
 
     def __str__(self):
         return f"{self.name} expires on {self.expiry}"
@@ -15,8 +65,3 @@ class Food(mongo_db.Document):
 
     def has_expired(self):
         return (self.expiry - datetime.now()).days < 0
-
-    def get_recipe(self):
-        return receipe_lookup(self.name)
-
-
--- backend/views.py
+++ backend/views.py
@@ -1,6 +1,6 @@
 import json
 from bson import json_util
-from flask import render_template, request, redirect, abort, jsonify
+from flask import render_template, request, redirect, abort, jsonify, url_for
 from . import app, mongo_db
 from .models import Food
 
@@ -13,14 +13,14 @@ def show_foods():
 
 @app.route('/api/delete_food/<id>')
 def delete_food(id):
-    Food.objects.get_or_404(_id = id)
+    Food.objects.get_or_404(_id=id)
     if Food.objects(_id=id).count() == 0:
         return "Item deleted successfully"
     else:
         return f"Error deleting item with id:{id}"
 
 
-@app.route('/api/get_food/<name>', methods=['GET'])
+@app.route('/api/food/<name>', methods=['GET'])
 def get_food(name):
     food = Food.objects.get_or_404(name=name)
     if food:
@@ -29,7 +29,7 @@ def get_food(name):
         return abort(404)
 
 
-@app.route('/api/get_food/<name>/recipes', methods=['GET'])
+@app.route('/api/food/<name>/recipes', methods=['GET'])
 def get_recipe_for_food(name):
     food = Food.objects.get_or_404(name=name)
     if food:
@@ -38,25 +38,54 @@ def get_recipe_for_food(name):
         return abort(404)
 
 
-@app.route('/set_food', methods=['POST'])
-def set_food_form():
-    new_food = Food(name=request.form['name'], expiry=request.form['expiration_date'])
-    new_food.save()
-    return redirect('/foods')
+@app.route("/api/food/remove/", method=["POST"])
+def remove_food_from_db():
+    delete_food_dict = json.loads(request.data)
+    food = Food.objects.get_or_404(_id=delete_food_dict["id"])
+    food.delete()
+    if Food.objects(_id=delete_food_dict["id"]).count() == 0:
+        return jsonify({'result': 1})
+    else:
+        return abort(500)
 
 
+# @app.route('/set_food', methods=['POST'])
+# def set_food_form():
+#     new_food = Food(name=request.form['name'])
+#
+#     new_food.save()
+#     return redirect('/foods')
+@app.route('/api/barcode/<int:barcode>')
+def get_food_from_barcode(barcode):
+    food = Food.objects(barcode=barcode)
+    if food:
+        return food.to_json()
+    else:
+        #Query service to lookup product from DB
+    return "Can't find item for barcode"
 
-@app.route('/api/set_food/', methods=['POST'])
+
+@app.route('/app/food/<int:id>/co2',method=['POST'])
+def estimate_co2_consumption(id):
+    location_data = json.load(request.data)
+    food = Food.objects.get_or_404(_id=id)
+    if location_data:
+
+
+@app.route('/api/food/', methods=['POST'])
 def set_food():
     if request.data:
         new_food_dict = json.loads(request.data)
-        new_food = Food(name=new_food_dict['name'], expiry=new_food_dict['expiry'])
+        new_food = Food(name=new_food_dict['name'])
+        for item in dir(Food):
+            if item in new_food_dict:
+                setattr(new_food, item, new_food_dict[item])
         new_food.save()
         if Food.objects(id=new_food.id).count() > 0:
             return "Successfully created item"
     return abort(500)
 
 
-@app.route('/login')
+@app.route('/login', methods=["POST", "GET"])
 def show_login():
     return render_template("login.html")
