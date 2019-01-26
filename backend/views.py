429ea1cb9962def91c8663ac08cd619cebd19bd5
Robert <robertkng104@gmail.com>
2019-01-26 14:59:51 +0000

    Update things

--- backend/views.py
+++ backend/views.py
@@ -1,20 +1,21 @@
-from flask import jsonify
+from flask import jsonify,render_template
 from . import app, mongo_db
 
 
-@app.route('/')
-def hello():
-    return "Hello world!"
+@app.route('/foods')
+def show_foods():
+    foods = mongo_db.db.foods.find({})
+    return render_template("foods.html",foods = foods)
 
 
-@app.route('/api/get_food/<name>',method=['GET'])
+@app.route('/api/get_food/<name>',methods=['GET'])
 def get_food(name):
     food = mongo_db.db.foods.find_one({'name': name})
     food.pop('_id')
     return jsonify(food)
 
 
-@app.route('/api/set_food/',method=['POST'])
+@app.route('/api/set_food/',methods=['POST'])
 def set_food():
     foods = mongo_db.db.foods
     new_food = {
