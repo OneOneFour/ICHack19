1075fc36b31f8dccc9fb18b631a78a6b0509b51c
Robert <robertkng104@gmail.com>
2019-01-26 15:28:11 +0000

    Added form to update

--- backend/views.py
+++ backend/views.py
@@ -1,27 +1,39 @@
-from flask import jsonify,render_template
+import json
+from flask import jsonify, render_template, request, redirect
 from . import app, mongo_db
 
 
 @app.route('/foods')
 def show_foods():
     foods = mongo_db.db.foods.find({})
-    return render_template("foods.html",foods = foods)
+    return render_template("foods.html", foods=foods)
 
 
-@app.route('/api/get_food/<name>',methods=['GET'])
+@app.route('/api/get_food/<name>', methods=['GET'])
 def get_food(name):
     food = mongo_db.db.foods.find_one({'name': name})
     food.pop('_id')
     return jsonify(food)
 
 
-@app.route('/api/set_food/',methods=['POST'])
+@app.route('/set_food', methods=['POST'])
+def set_food_form():
+    new_food = {
+        'name': request.form['name'],
+        'expiry_date_min': request.form['expiry_date_min'],
+        'expiry_date_max': request.form['expiry_date_max']
+    }
+    mongo_db.db.foods.insert(new_food)
+    return redirect('/foods')
+
+
+@app.route('/api/set_food/', methods=['POST'])
 def set_food():
     foods = mongo_db.db.foods
-    new_food = {
-        'name': "Cake",
-        'expiry_date_min': 2,
-        'expiry_date_max': 3
-    }
-    foods.insert(new_food)
-    return "added item"
+    new_food = request.data
+    if new_food:
+        new_food_dict = json.loads(new_food)
+        foods.insert(new_food_dict)
+        if foods.find_one({'name': new_food_dict['name']}).count() > 0:
+            return "Successfully created item"
+    return "Something went wrong!"
