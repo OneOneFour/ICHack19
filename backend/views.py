63f37b146b41ba980af60e19057576ce4d5e8328
Robert <robertkng104@gmail.com>
2019-01-26 19:43:20 +0000

    more stuff

--- backend/views.py
+++ backend/views.py
@@ -1,6 +1,6 @@
 import json
 from bson import json_util
-from flask import render_template, request, redirect, abort
+from flask import render_template, request, redirect, abort, jsonify
 from . import app, mongo_db
 from .models import Food
 
@@ -13,8 +13,8 @@ def show_foods():
 
 @app.route('/api/delete_food/<id>')
 def delete_food(id):
-    Food.objects(id=id).delete()
-    if Food.objects(id=id).count() == 0:
+    Food.objects.get_or_404(_id = id)
+    if Food.objects(_id=id).count() == 0:
         return "Item deleted successfully"
     else:
         return f"Error deleting item with id:{id}"
@@ -22,9 +22,18 @@ def delete_food(id):
 
 @app.route('/api/get_food/<name>', methods=['GET'])
 def get_food(name):
-    food = Food.objects(name=name)
+    food = Food.objects.get_or_404(name=name)
     if food:
         return food.to_json()
+    else:
+        return abort(404)
+
+
+@app.route('/api/get_food/<name>/recipes', methods=['GET'])
+def get_recipe_for_food(name):
+    food = Food.objects.get_or_404(name=name)
+    if food:
+        return jsonify(food.get_recipe())
     else:
         return abort(404)
 
@@ -36,16 +45,16 @@ def set_food_form():
     return redirect('/foods')
 
 
+
 @app.route('/api/set_food/', methods=['POST'])
 def set_food():
     if request.data:
         new_food_dict = json.loads(request.data)
-        new_food = Food(name = new_food_dict['name'],expiry=new_food_dict['expiry'])
+        new_food = Food(name=new_food_dict['name'], expiry=new_food_dict['expiry'])
         new_food.save()
-        if Food.objects(id = new_food.id).count() > 0:
+        if Food.objects(id=new_food.id).count() > 0:
             return "Successfully created item"
     return abort(500)
-
 
 
 @app.route('/login')
