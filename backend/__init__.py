8e51b058e54aaad6156c4e351512660979789a58
Robert <robertkng104@gmail.com>
2019-01-26 16:58:50 +0000

    Added comments

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,12 +1,19 @@
 from flask import Flask
 from flask_pymongo import PyMongo
-
+from flask_login import LoginManager
+#Create flask app
 app = Flask(__name__)
+
+#Connect to DB
 app.config["MONGO_URI"] = "mongodb://localhost:27017/foods"
 mongo_db = PyMongo(app)
 
+#Creating login instance
+login = LoginManager(app)
+
 DEBUG = True
 
+#We import views after setting things up but BEFORE app.run to ensure the functions for the views have all been defined in time
 from .views import *
 
 if __name__ == "__main__":
