c892f39d45f1333664f3f652e3a57f3d54dc361c
Robert <robertkng104@gmail.com>
2019-01-27 10:53:21 +0000

    Changed number of recipes requested

--- edmam_wrapper.py
+++ edmam_wrapper.py
@@ -6,7 +6,7 @@ intresting_keys = ("label","image", "url", "calories", "healthLabels")
 
 
 def receipe_lookup(query):
-    params = {"app_id": app.config['EDAMAM_APP_ID'], "app_key": app.config['EDAMAM_APP_AUTH'], "q": query,"to":2}
+    params = {"app_id": app.config['EDAMAM_APP_ID'], "app_key": app.config['EDAMAM_APP_AUTH'], "q": query,"to":5}
     r = requests.get("https://api.edamam.com/search", params)
     if r.status_code == 200:
         return [{k: hit["recipe"][k] for k in intresting_keys} for hit in json.loads(r.text)["hits"]]
